27f2fb50815ce79676b5044af1a58bdaf5803829
Souradip <anihero4@gmail.com>
2020-07-18 23:40:00 +0530

    Suspense Commit with Loading

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -3,7 +3,10 @@ import React from 'react';
 
 function Loading(){
     return(
-        <div>Loading...</div>
+        <div className="col-12 col-md-10 body text-center">
+            <h2>Thanks For Keeping Patience</h2>
+            <h4>Loading . . .</h4>
+        </div>
     );
 }
 
