93013e2a391a2d14a7a384c376e447f6d84168bc
Souradip <anihero4@gmail.com>
2020-07-18 23:02:38 +0530

    Suspense Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './App.css';
-import Loadable from 'react-loadable'; 
+//import Loadable from 'react-loadable'; 
+import Main from './components/MainComponent';
 import {BrowserRouter} from 'react-router-dom';
+import Loading from './components/LoadingComponent';
 
-const LoadableMain = Loadable({
-  loader: () => import('./components/MainComponent'),
-  loading: () => <div>Loading ...</div>
-});
+// const LoadableMain = Loadable({
+//   loader: () => import('./components/MainComponent'),
+//   loading: () => <div>Loading ...</div>
+// });
 
 function App() {
 
   return (
     <BrowserRouter>
       <div>
-          <LoadableMain />
+        <Suspense fallback={<Loading/>}>
+          <Main />
+        </Suspense>
       </div>
     </BrowserRouter>
   );
