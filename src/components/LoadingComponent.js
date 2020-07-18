0eb9f3a994e431378188f6e1e69fd89a10424037
Souradip <anihero4@gmail.com>
2020-07-19 00:06:25 +0530

    Suspense Commit with Loading

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -4,8 +4,10 @@ import React from 'react';
 function Loading(){
     return(
         <div className="col-12 col-md-10 body text-center">
-            <h2>Thanks For Keeping Patience</h2>
-            <h4>Loading . . .</h4>
+            <span className="fa fa-spinner fa-pulse fa-3x fa-fw text-primary"></span>
+                
+            <h4>Thanks For Keeping Patience</h4>
+            <h5>Loading . . .</h5>
         </div>
     );
 }
